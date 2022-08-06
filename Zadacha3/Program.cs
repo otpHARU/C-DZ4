f09862b8e61403f8135c045a40a29bf2365dca0d
Haru <harumolodec@mail.ru>
2022-08-06 11:30:01 +0700

    Zadacha3 END

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = GetBinaryArray(8);
+// Console.Write($"[{String.Join(" , ", array)}]");
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(100);
+        Console.Write("{0} ", result[i]);
+    }
+    return result;
+}
