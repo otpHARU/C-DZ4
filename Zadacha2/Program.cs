c68aad522e48c99a3acdb8191648786edd35ecf1
Haru <harumolodec@mail.ru>
2022-08-06 11:02:06 +0700

    Zadacha2 END

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+while (numberN > 0)
+{
+    result += numberN % 10;
+    numberN /= 10;
+}
+
+Console.WriteLine("Сумма цифр в числе = " + result);
+
+
+
+
+
+
+
+
+
+
+
